27c760fce9fc1883f0352288a4f7dc113c98b4e1
Dmitry Pautov <111112223344az@gmail.com>
2020-07-07 22:58:53 +0300

    Add files via upload

--- pautov1300/src/App.js
+++ pautov1300/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './styles/Main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './components/Hight/Layout';
 import Main from './components/Hight/Main';
